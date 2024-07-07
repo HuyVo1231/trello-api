a5b25ca8a13b0f26db55c13edfb02032e937ee33
HuyVo1231 <vonhathuy1306@gmail.com>
2024-07-07 09:05:35 +0700

    Deploy change Whitelist_domains 2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const WHITELIST_DOMAINS = ['https://mern-trello-3ljicqzy4-huyvo1231s-projects.vercel.app/']
+export const WHITELIST_DOMAINS = ['https://mern-trello-3ljicqzy4-huyvo1231s-projects.vercel.app']
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
