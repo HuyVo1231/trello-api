3ea8470a2dee379f00fe82e6e1194fd7f88eb2a6
HuyVo1231 <vonhathuy1306@gmail.com>
2024-07-17 07:10:02 +0700

    Fix whitelist

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
-export const WHITELIST_DOMAINS = ['https://mern-trello-3ljicqzy4-huyvo1231s-projects.vercel.app']
+export const WHITELIST_DOMAINS = [
+  'https://mern-trello-3ljicqzy4-huyvo1231s-projects.vercel.app',
+  'https://mern-trello-fryh1ep36-huyvo1231s-projects.vercel.app'
+]
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
